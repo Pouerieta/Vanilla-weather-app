66af8adc196bc3ba1a538f8251cef0259ccdf3de
Pouerieta <ashleypoueriet@gmail.com>
2023-10-25 14:46:43 -0400

    Im having trouble with descriptions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-function displayTemperature(response) {
+function showTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
-  temperatureElement.innerHTML = Math.round(response.data.main);
-  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "8476te1bf3d09d0bo93fc14adf032bbf";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New York&key=8476te1bf3d09d0bo93fc14adf032bbf`;
-axios.get(apiUrl).then(displayTemperature);
+
+axios.get(apiUrl).then(showTemperature);
